4e80b2f15668da9020d317313eb9c39688902c54
Denis Sorokin <denys-s@yit.co.il>
2017-01-24 08:22:41 +0200

    launch

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 "use strict";
-var term = require("terminal-kit").terminal;
 var Point_1 = require("./Point");
 var HorizontalLine_1 = require("./HorizontalLine");
 var VerticalLine_1 = require("./VerticalLine");
-var p01 = new Point_1["default"](2, 1, "*");
-var p02 = new Point_1["default"](8, 10, "#");
-var hl = new HorizontalLine_1["default"](2, 6, 3, "@");
-var vl = new VerticalLine_1["default"](4, 7, 2, "+");
-p01.Draw();
-hl.Draw();
-vl.Draw();
+// var child_process = require('child_process')
+// child_process.execSync("start cmd.exe /K node app.js ")
+var p02 = new Point_1["default"](3, 3, "#");
+var pReset = new Point_1["default"](0, 11, "");
+var hlTop = new HorizontalLine_1["default"](0, 20, 0, "+");
+var hlBot = new HorizontalLine_1["default"](0, 20, 10, "+");
+var vlTop = new VerticalLine_1["default"](0, 9, 1, "+");
+var vlBot = new VerticalLine_1["default"](0, 9, 20, "+");
+hlTop.Draw();
+hlBot.Draw();
+vlTop.Draw();
+vlBot.Draw();
 p02.Draw();
+pReset.Draw();
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-let term = require("terminal-kit").terminal
 import Point from "./Point"
 import HorizontalLine from "./HorizontalLine"
 import VerticalLine from './VerticalLine';
 
-let p01 = new Point(2, 1, "*")
-let p02 = new Point(8, 10, "#")
-let hl = new HorizontalLine(2, 6, 3, "@")
-let vl = new VerticalLine(4,7,2,"+")
-p01.Draw()
-hl.Draw()
-vl.Draw()
+// var child_process = require('child_process')
+// child_process.execSync("start cmd.exe /K node app.js ")
+
+
+let p02 = new Point(3, 3, "#")
+let pReset = new Point(0, 11, "")
+let hlTop = new HorizontalLine(0, 20, 0, "+")
+let hlBot = new HorizontalLine(0, 20, 10, "+")
+let vlTop = new VerticalLine(0,9,1,"+")
+let vlBot = new VerticalLine(0,9,20,"+")
+hlTop.Draw()
+hlBot.Draw()
+vlTop.Draw()
+vlBot.Draw()
 p02.Draw()
+pReset.Draw()
